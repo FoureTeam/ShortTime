a49b6a95aa8e3a73552ff96850e6a1461cc3f1b7
xiaodongmemeda <344650464@qq.com>
2017-07-21 19:42:19 +0800

    bottomnavigationbar

--- app/src/main/java/com/QHTtest/view/activity/HomeActivity.java
+++ app/src/main/java/com/QHTtest/view/activity/HomeActivity.java
@@ -2,7 +2,10 @@ package com.QHTtest.view.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -11,6 +14,7 @@ import com.QHTtest.R;
 import com.QHTtest.presenter.HomePresenter;
 import com.QHTtest.view.adapter.HomeRecyLeftAda;
 import com.QHTtest.view.fragment.CrossTalkFrag;
+import com.QHTtest.view.fragment.DrawerFrag;
 import com.QHTtest.view.fragment.RecommendFrag;
 import com.QHTtest.view.fragment.VideoFrag;
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
@@ -22,7 +26,7 @@ import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class HomeActivity extends BaseActivity<HomePresenter> {
+public class HomeActivity extends BaseActivity<HomePresenter> implements BottomNavigationBar.OnTabSelectedListener {
 
     private RecyclerView class_frag_recy;
     private Map<String, String> map = new HashMap<>();
@@ -37,6 +41,8 @@ public class HomeActivity extends BaseActivity<HomePresenter> {
     private CrossTalkFrag crossTalkFrag;
     private VideoFrag videoFrag;
     private ArrayList<Fragment> fragments;
+    private Toolbar home_toolbar;
+    private DrawerLayout home_drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +64,10 @@ public class HomeActivity extends BaseActivity<HomePresenter> {
     protected void initView() {
 
         //初始化控件
+
+        home_toolbar = (Toolbar) findViewById(R.id.layout_toolbar);
+        home_drawer = (DrawerLayout) findViewById(R.id.layout_drawer);
+
         home_toolbar_head_img = (CircleImageView) findViewById(R.id.home_toolbar_head_img);
         home_toolbar_text = (TextView) findViewById(R.id.home_toolbar_text);
         home_toolbar_edit_img = (ImageView) findViewById(R.id.home_toolbar_edit_img);
@@ -70,6 +80,62 @@ public class HomeActivity extends BaseActivity<HomePresenter> {
 
     @Override
     protected void initData() {
+
+
+        //初始化fragment
+        recommendFrag = new RecommendFrag();
+        crossTalkFrag = new CrossTalkFrag();
+        videoFrag = new VideoFrag();
+
+        fragments = new ArrayList<>();
+        fragments.add(recommendFrag);
+        fragments.add(crossTalkFrag);
+        fragments.add(videoFrag);
+
+        setSupportActionBar(home_toolbar);
+
+        home_drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+
+        home_drawer.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                home_drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                home_drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                super.onDrawerStateChanged(newState);
+            }
+        });
+
+        //初始化navigationbar
+        initBottomNavigationBar();
+
+        initDrawerFragment();
+
+    }
+
+
+    /**
+     * 初始化侧滑页面
+     */
+    private void initDrawerFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.home_startDrawerLayout,new DrawerFrag())
+                .commitAllowingStateLoss();
+    }
+
+    private void initBottomNavigationBar() {
         //设置模式：普通模式
         navigationbar.setMode(BottomNavigationBar.MODE_FIXED);
 
@@ -100,21 +166,14 @@ public class HomeActivity extends BaseActivity<HomePresenter> {
 
         home_toolbar_text.setText("推荐");
 
-        //初始化fragment
-        recommendFrag = new RecommendFrag();
-        crossTalkFrag = new CrossTalkFrag();
-        videoFrag = new VideoFrag();
-
-        fragments = new ArrayList<>();
-        fragments.add(recommendFrag);
-        fragments.add(crossTalkFrag);
-        fragments.add(videoFrag);
-
-
-
-
-
-
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.home_rela, recommendFrag)
+                .add(R.id.home_rela, crossTalkFrag)
+                .add(R.id.home_rela, videoFrag)
+                .hide(fragments.get(1))
+                .hide(fragments.get(2))
+                .commitAllowingStateLoss();
+        navigationbar.setTabSelectedListener(this);
     }
 
     @Override
@@ -122,5 +181,38 @@ public class HomeActivity extends BaseActivity<HomePresenter> {
         return R.layout.activity_main;
     }
 
+    int tag = 0;
 
+    @Override
+    public void onTabSelected(int position) {
+        switch (position){
+            case 0:
+                home_toolbar_text.setText("推荐");
+
+                break;
+            case 1:
+                home_toolbar_text.setText("段子");
+
+                break;
+            case 2:
+                home_toolbar_text.setText("视频");
+
+                break;
+        }
+
+        if (tag != position){
+            getSupportFragmentManager().beginTransaction().show(fragments.get(position)).hide(fragments.get(tag)).commitAllowingStateLoss();
+            tag = position;
+        }
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
+    }
 }
--- app/src/main/java/com/QHTtest/view/fragment/RecommendFrag.java
+++ app/src/main/java/com/QHTtest/view/fragment/RecommendFrag.java
@@ -2,9 +2,13 @@ package com.QHTtest.view.fragment;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.QHTtest.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by 仇海涛 on 2017/7/21.
@@ -15,13 +19,20 @@ import com.QHTtest.R;
 public class RecommendFrag extends BaseFragment {
 
 
-    private TabLayout recommentFragVp;
+    private ViewPager recommentFragVp;
     private TabLayout recommentFragTb;
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private ArrayList<String> strings = new ArrayList<>();
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
-//        recommentFragVp = view.findViewById(R.id.recommentfrag_viewpager);
-//        recommentFragTb = view.findViewById(R.id.recommentfrag_tablayout);
+        //初始化控件
+        recommentFragVp = view.findViewById(R.id.recommentfrag_viewpager);
+        recommentFragTb = view.findViewById(R.id.recommentfrag_tablayout);
+
+        strings.add("热门");
+        strings.add("关注");
+
     }
 
     @Override
