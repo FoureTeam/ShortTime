074649905463026a8d76d9aca738a8a932af2c22
PeotryFavour <3473715672@qq.com>
2017-07-31 10:59:53 +0800

    Merge branch 'master' of https://github.com/FoureTeam/ShortTime
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/QHTtest/view/activity/AttentionActivity.java
+++ app/src/main/java/com/QHTtest/view/activity/AttentionActivity.java
@@ -8,6 +8,9 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.QHTtest.R;
 import com.QHTtest.presenter.PostPresenter;
@@ -24,12 +27,14 @@ import java.util.List;
  * 作者：邹诗惠 on 2017/7/28 17:29
  */
 
-public class AttentionActivity extends BaseActivity<PostPresenter> {
+public class AttentionActivity extends BaseActivity<PostPresenter> implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private SwipeRefreshLayout swipe;
     private AttentionRecyleAdapter adapter;
     private List<String> s;
+    private TextView back;
+    private TextView hot;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -41,7 +46,8 @@ public class AttentionActivity extends BaseActivity<PostPresenter> {
     @Override
     protected void initListener() {
         initPullRefresh();
-
+        back.setOnClickListener(this);
+        hot.setOnClickListener(this);
     }
 
     @Override
@@ -53,21 +59,35 @@ public class AttentionActivity extends BaseActivity<PostPresenter> {
     protected void initView() {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView_atte);
         swipe = (SwipeRefreshLayout) findViewById(R.id.swipe_atte);
-        recyclerView.addItemDecoration(new DividerView(AttentionActivity.this));
-        swipe.setColorSchemeColors(Color.BLUE, Color.BLACK,
-                Color.RED, Color.YELLOW);
+        back = (TextView) findViewById(R.id.left_my);
+        hot = (TextView) findViewById(R.id.right_my);
+        hot.setText("热门关注");
+    }
+
+    @Override
+    protected void initData() {
         initHttpT();
         initHttp();
+    }
 
+    @Override
+    public int getLayout() {
+        return R.layout.activity_attention;
     }
 
     private void initHttpT() {
+        //添加自定义分割线
+        recyclerView.addItemDecoration(new DividerView(AttentionActivity.this));
+        //设置下拉刷新颜色
+        swipe.setColorSchemeColors(Color.BLUE, Color.BLACK,
+                Color.RED, Color.YELLOW);
         //创建适配器
         adapter = new AttentionRecyleAdapter(this);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1);
         recyclerView.setLayoutManager(gridLayoutManager);
     }
 
+    //添加虚拟数据
     private void initHttp() {
         s = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
@@ -76,16 +96,6 @@ public class AttentionActivity extends BaseActivity<PostPresenter> {
         adapter.setData(s);
         recyclerView.setAdapter(adapter);
 
-    }
-
-    @Override
-    protected void initData() {
-
-    }
-
-    @Override
-    public int getLayout() {
-        return R.layout.activity_attention;
     }
 
     //上拉刷新
@@ -110,4 +120,18 @@ public class AttentionActivity extends BaseActivity<PostPresenter> {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.left_my: {
+                Toast.makeText(AttentionActivity.this,"返回",Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case R.id.right_my: {
+                Toast.makeText(AttentionActivity.this,"热门关注",Toast.LENGTH_SHORT).show();
+            }
+            break;
+
+        }
+    }
 }
